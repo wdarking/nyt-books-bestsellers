2c4a422b6a41ee258d1fdef4b66688d14007c497
André Pereira <andrepereirapink@gmail.com>
2023-07-05 17:37:54 -0300

    setup project structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="h-screen grid place-items-center">
+      <div className="container mx-auto p-3">
+        <h1 className="text-2xl font-medium text-center">
+          Hello NYC Bestsellers!
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
