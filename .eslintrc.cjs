fc458a0ffb9c7c09ba78ebaa31d7bba63d0717b9
André Pereira <andrepereirapink@gmail.com>
2023-07-11 16:34:16 -0300

    chore: fix linting options & prettier conflicts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,16 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  plugins: ["react-refresh"],
+  plugins: ["react", "react-refresh", "prettier"],
   rules: {
     "react-refresh/only-export-components": "warn",
+    "prettier/prettier": ["error"],
+    "react/react-in-jsx-scope": "off",
   },
 };
