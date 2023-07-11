f4b34b7c827345b408ee0361c575e7d5b9cad33a
André Pereira <andrepereirapink@gmail.com>
2023-07-10 21:21:41 -0300

    setup initial layout structure & react-query (wip)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { CategoryList } from "./components/books/CategoryList";
+import { useState } from "react";
+import { BooksList } from "./components/books/BooksList";
+
+const queryClient = new QueryClient();
+
 function App() {
+  const [selectedList, setSelectedList] = useState("hardcover-fiction");
   return (
-    <div className="h-screen grid place-items-center">
-      <div className="container mx-auto p-3">
-        <h1 className="text-2xl font-medium text-center">
-          Hello NYC Bestsellers!
-        </h1>
+    <QueryClientProvider client={queryClient}>
+      <div className="h-screen grid">
+        <div className="container mx-auto p-3 flex flex-col">
+          <h1 className="text-2xl font-medium text-center mb-3">
+            Hello NYC Bestsellers!
+          </h1>
+          <div className="flex flex-col md:flex-row gap-3 md:gap-4 flex-1">
+            <div className="md:w-[300px]">
+              <CategoryList setSelectedList={setSelectedList} />
+            </div>
+            <div className="flex-1">
+              <BooksList listSlug={selectedList} />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   );
 }
 
