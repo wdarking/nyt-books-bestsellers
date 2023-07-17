b34a60110b717f8c91084e1f1ed4ba89258d6dd6
André Pereira <andrepereirapink@gmail.com>
2023-07-16 22:13:47 -0300

    refactor: extract Header & footer to components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { CategoryList } from "./components/books/CategoryList";
 import { useState } from "react";
 import { BooksList } from "./components/books/BooksList";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
 const queryClient = new QueryClient();
 
@@ -9,11 +11,9 @@ function App() {
   const [selectedList, setSelectedList] = useState("hardcover-fiction");
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="min-h-screen grid bg-slate-100">
-        <div className="container mx-auto p-3 flex flex-col">
-          <h1 className="text-2xl font-medium text-center mb-3">
-            Hello NYC Bestsellers!
-          </h1>
+      <div className="min-h-screen grid grid-rows-[min-content_1fr_min-content] bg-slate-100 space-y-5 md:space-y-10">
+        <Header />
+        <div className="container mx-auto px-3 flex flex-col">
           <div className="flex flex-col md:flex-row gap-3 md:gap-4 flex-1">
             <div className="md:w-[300px]">
               <CategoryList setSelectedList={setSelectedList} />
@@ -21,8 +21,10 @@ function App() {
             <div className="flex-1 md:px-5">
               <BooksList listSlug={selectedList} />
             </div>
+            <div className="md:w-[300px]"></div>
           </div>
         </div>
+        <Footer />
       </div>
     </QueryClientProvider>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+  return (
+    <header className="bg-white">
+      <div className="container mx-auto py-5 px-3">
+        <span className="font-serif font-bold tracking-wider text-xl border-b">
+          NYT Best Sellers
+        </span>
+      </div>
+    </header>
+  );
+}
