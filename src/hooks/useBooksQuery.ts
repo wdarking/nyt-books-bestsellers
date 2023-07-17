9b138e4408fc2df5ae5bf28cf9ab119cc337d5ce
André Pereira <andrepereirapink@gmail.com>
2023-07-16 22:21:53 -0300

    feat: add new query hooks refactor: reformulate and extract api types

--- src/hooks/useBooksQuery.ts
+++ src/hooks/useBooksQuery.ts
@@ -1,11 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
-import { fetchBooksByList, fetchListCategories } from "../services/booksApi";
+import {
+  fetchBookHistory,
+  fetchBooksByList,
+  fetchListByDate,
+  fetchListCategories,
+} from "../services/booksApi";
 
 export function useListCategoriesQuery() {
   return useQuery({
     queryKey: ["list-names"],
     queryFn: fetchListCategories,
-    staleTime: Infinity,
   });
 }
 
@@ -13,6 +17,21 @@ export function useBooksByListQuery(slug: string) {
   return useQuery({
     queryKey: ["books", slug],
     queryFn: () => fetchBooksByList(slug),
-    staleTime: Infinity,
+  });
+}
+
+export function useBooksByDateQuery(slug: string) {
+  const now = new Date();
+  const latestDate = now.toISOString().split("T")[0];
+  return useQuery({
+    queryKey: ["books", slug, latestDate],
+    queryFn: () => fetchListByDate(slug, latestDate),
+  });
+}
+
+export function useBooksHistoryQuery(isbn: string) {
+  return useQuery({
+    queryKey: ["book-history", isbn],
+    queryFn: () => fetchBookHistory(isbn),
   });
 }
--- src/services/booksApi.ts
+++ src/services/booksApi.ts
@@ -1,4 +1,10 @@
 import axios from "axios";
+import {
+  BookHistoryResponse,
+  ListByDateResponse,
+  ListCategoriesResponse,
+  ListResponse,
+} from "./booksApiTypes";
 
 const baseURL = "https://api.nytimes.com/svc/books/v3/";
 const apiKey = import.meta.env.VITE_API_KEY;
@@ -10,76 +16,37 @@ const api = axios.create({
   },
 });
 
-type OverviewSchema<ResultType> = {
-  status: string;
-  copyright: string;
-  num_results: number;
-  results: ResultType[];
-};
-
-type ListNameType = {
-  list_name: string;
-  display_name: string;
-  list_name_encoded: string;
-  oldest_published_date: Date;
-  newest_published_data: Date;
-  updated: string;
-};
-
-type ListCategoriesResponse = Promise<OverviewSchema<ListNameType>>;
-
 export async function fetchListCategories(): ListCategoriesResponse {
   const { data } = await api.get("lists/names.json");
   return data;
 }
 
-type ISBN = {
-  isbn10: string;
-  isbn13: string;
-};
-
-export type BookDetailsSchema = {
-  title: string;
-  description: string;
-  contributor: string;
-  author: string;
-  contributor_note: string;
-  price: number;
-  age_group: string;
-  publisher: string;
-  primary_isbn13: string;
-  primary_isbn10: string;
-};
-
-type ReviewSchema = {
-  book_review_link: string;
-  first_chapter_link: string;
-  sunday_review_link: string;
-  article_chapter_link: string;
-};
-
-export type BestSellerSchema = {
-  list_name: string;
-  display_name: string;
-  bestsellers_date: string;
-  published_date: string;
-  rank: number;
-  rank_last_week: number;
-  weeks_on_list: number;
-  asterisk: number;
-  dagger: number;
-  amazon_product_url: string;
-  isbns: ISBN[];
-  book_details: BookDetailsSchema[];
-  reviews: ReviewSchema[];
-};
-
-type BestSellersResponse = Promise<OverviewSchema<BestSellerSchema>>;
-
-export async function fetchBooksByList(slug: string): BestSellersResponse {
+export async function fetchBooksByList(slug: string): ListResponse {
   const { data } = await api.get("lists.json", {
     params: {
       list: slug,
+    },
+  });
+
+  console.log(data);
+
+  return data;
+}
+
+export async function fetchListByDate(
+  list: string,
+  date: string,
+): ListByDateResponse {
+  const { data } = await api.get(`lists/${date}/${list}.json`);
+
+  console.log(data);
+  return data;
+}
+
+export async function fetchBookHistory(isbn: string): BookHistoryResponse {
+  const { data } = await api.get("lists/best-sellers/history.json", {
+    params: {
+      isbn,
     },
   });
 
