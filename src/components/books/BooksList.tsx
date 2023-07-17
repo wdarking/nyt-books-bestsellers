0109cc13dde26080e59ea78a97585f54b8686ae6
André Pereira <andrepereirapink@gmail.com>
2023-07-16 22:26:50 -0300

    feat(lists): add loading skeleton animation feat: add list name & details as heading

--- src/components/books/BooksList.tsx
+++ src/components/books/BooksList.tsx
@@ -1,11 +1,11 @@
-import { useBooksByListQuery } from "../../hooks/useBooksQuery";
+import { useBooksByDateQuery } from "../../hooks/useBooksQuery";
 import { Book } from "./Book";
 
 export function BooksList({ listSlug }: { listSlug: string }) {
-  const { data, isLoading, isError } = useBooksByListQuery(listSlug);
+  const { data, isLoading, isError } = useBooksByDateQuery(listSlug);
 
   if (isLoading) {
-    return "Loading...";
+    return <ListSkeleton count={5} />;
   }
 
   if (isError) {
@@ -13,11 +13,30 @@ export function BooksList({ listSlug }: { listSlug: string }) {
   }
 
   return (
-    <ul className="grid grid-cols-1 auto-rows-max gap-3 md:gap-4">
-      {data.results.map((bestseller) => (
-        <li key={bestseller.book_details[0].primary_isbn13}>
-          <Book {...bestseller} />
-        </li>
+    <>
+      <div className="mb-3 md:mb-5">
+        <h1 className="text-2xl font-bold ">{data.results.display_name}</h1>
+        <p className="text-slate-500">Updated {data.results.updated}</p>
+      </div>
+      <ul className="grid grid-cols-1 auto-rows-max gap-3 md:gap-4">
+        {data.results.books.map((book) => (
+          <li key={book.primary_isbn13}>
+            <Book book={book} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export function ListSkeleton({ count = 1 }: { count: number }) {
+  return (
+    <ul className="space-y-3 md:space-y-5">
+      {Array.from({ length: count }).map((_, index) => (
+        <div
+          key={index}
+          className="bg-slate-200 w-full h-20 md:h-28 rounded animate-pulse"
+        ></div>
       ))}
     </ul>
   );
